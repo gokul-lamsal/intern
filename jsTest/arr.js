f2eb8b7f19201a393caefa3f044662b34c50b517
rowanhere <lamsalgokul917@gmail.com>
2026-08-21 12:46:27 +0545

    array problem solved

---
+++ jsTest/arr.js
@@ -0,0 +1,11 @@
+const arr1 = [1, 2, 3, 4, 5, 6];
+const arr2 = [88, 5, 7, 99, 66, 9];
+let arr3 = [];
+for (let i = 0; i < arr1.length; i++) {
+  arr3[i] = arr1[i];
+}
+for (let i = 0; i < arr2.length; i++) {
+  arr3[i + arr1.length] = arr2[i];
+}
+
+console.log(arr3);
